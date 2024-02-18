ede3e1ef4d68965a8624d223c5184b1e401099b2
Rebecca Truesdale <rebecca.truesdale@gmail.com>
2024-02-18 18:28:39 +0000

    build

--- Assets/Scripts/testing/mouseposition.cs
+++ Assets/Scripts/testing/mouseposition.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEditor;
+// using UnityEngine;
 
-[InitializeOnLoad]
-public class mouseposition : MonoBehaviour
-{
-    static mouseposition()
-    {
-        SceneView.duringSceneGui += OnSceneGUI;
-    }
+// [InitializeOnLoad]
+// public class mouseposition : MonoBehaviour
+// {
+//     static mouseposition()
+//     {
+//         SceneView.duringSceneGui += OnSceneGUI;
+//     }
 
-    static void OnSceneGUI(SceneView sceneView)
-    {
-        // Get the position of the mouse cursor in screen coordinates
-        Vector2 mousePosition = Event.current.mousePosition;
+//     static void OnSceneGUI(SceneView sceneView)
+//     {
+//         // Get the position of the mouse cursor in screen coordinates
+//         Vector2 mousePosition = Event.current.mousePosition;
 
-        // Convert the screen coordinates to world coordinates
-        Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
-        Vector3 worldPosition = ray.origin + ray.direction * (Camera.current.transform.position.y / -ray.direction.y);
+//         // Convert the screen coordinates to world coordinates
+//         Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
+//         Vector3 worldPosition = ray.origin + ray.direction * (Camera.current.transform.position.y / -ray.direction.y);
 
-        // Display the coordinates in the scene view
-        Handles.Label(worldPosition, "Mouse Position: " + worldPosition);
-    }
-}
+//         // Display the coordinates in the scene view
+//         Handles.Label(worldPosition, "Mouse Position: " + worldPosition);
+//     }
+// }
